830f710aa16a650ac9ac689279284fe9a5f000c0
Yan Kaliada <vetkaykropa@gmail.com>
2024-09-26 12:16:52 +0300

    some udemy + js

---
+++ hw2/hw2.1.js
@@ -0,0 +1,14 @@
+// 1.1 Напишите программу по согласованию отпуска в зависимости от месяца.
+//  Если месяц “July” или “August”, то программа должна показать - ‘approved’, 
+//  если другие месяцы, то - “denied”.
+
+
+
+
+let month = "July";
+
+if (month === "July" || month === "August") {
+  console.log('approved');
+} else {
+  console.log('denied');
+}
---
+++ udemy/logiOperators.js
@@ -0,0 +1,34 @@
+// const hamburger = 5;
+// const fries = null;
+
+// if (hamburger && fries) {
+//     console.log('Я ем жирную еду')
+// }
+// const hamburger = 3;
+// const fries = 1;
+// const cola = 0;
+
+// console.log(hamburger === 3 && cola && fries)
+// console.log(1 && 0)
+// console.log(1 && 5)
+// console.log(null && 5)
+// console.log(0 && 'asdasd1dsad')
+
+// if (hamburger === 3 && cola === 1 && fries) {
+//     console.log('Все сыты')
+// } else {
+//     console.log('Мы уходим отседова')
+// }
+
+
+const hamburger = 3;
+const fries = 1;
+const cola = 0;
+
+
+
+if (hamburger || cola || fries) {
+    console.log('Все сыты')
+} else {
+    console.log('Мы уходим отседова')
+}
